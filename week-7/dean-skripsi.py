eaddbc53b64c83957ab80ba250d76f26741c4c0b
kemal-faza <kemalfaza26@gmail.com>
2026-04-28 10:54:02 +0700

    Fix tumbler-gege and dean-skripsi with BFS solution

--- week-7/dean-skripsi.py
+++ week-7/dean-skripsi.py
@@ -1,21 +1,37 @@
 t = int(input())
 
-listNMS = []
-listPathTopik = []
-for i in range(t):
-    listNMS.append(list(map(int, input().split())))
-    pathTopik = []
-    for j in range(listNMS[i][1]):
-        pathTopik.append(list(map(int, input().split())))
-    listPathTopik.append(pathTopik)
-
-def dfs(indeks, visited, maxLangkah):
-    if 
-
-for i in range(len(listNMS)):
-    n, m, s = listNMS[0], listNMS[1], listNMS[2]
-    for j in range(n):
-        maxLangkah = 0
-        dfs(j, set(), maxLangkah)
-
-
+for _ in range(t):
+    n, m, s = map(int, input().split())
+    
+    adj = [[] for _ in range(n + 1)]
+    
+    for _ in range(m):
+        u, v = map(int, input().split())
+        adj[u].append(v)
+        adj[v].append(u)
+    
+    dist = [-1] * (n + 1)
+    dist[s] = 0
+    
+    queue = [s]
+    front = 0
+    while front < len(queue):
+        node = queue[front]
+        front += 1
+        
+        for neighbor in adj[node]:
+            if dist[neighbor] == -1:
+                dist[neighbor] = dist[node] + 1
+                queue.append(neighbor)
+    
+    max_dist = -1
+    farthest_node = n + 1
+    
+    for i in range(1, n + 1):
+        if dist[i] > max_dist:
+            max_dist = dist[i]
+            farthest_node = i
+        elif dist[i] == max_dist and i < farthest_node:
+            farthest_node = i
+    
+    print(farthest_node, max_dist)
--- week-7/tumbler-gege.py
+++ week-7/tumbler-gege.py
@@ -1,17 +1,14 @@
 n = int(input())
-listPi = list(map(int, input().split()))
+P = list(map(int, input().split()))
 
-
-def dfs(node, visited, index):
-    if node in visited:
-        print(node + 1)
-        return
-
-    visited.add(index)
-    if node not in visited:
-        dfs(listPi[node] - 1, visited, node)
-        return
-
-
-for i in range(n):
-    dfs(listPi[i] - 1, set(), i)
+for start in range(n):
+    visited = set()
+    current = start
+    
+    while True:
+        if current in visited:
+            print(current + 1)
+            break
+        
+        visited.add(current)
+        current = P[current] - 1
